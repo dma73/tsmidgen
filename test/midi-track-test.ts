77d36dfa444ece6c7f388814db982278b27f3c8a
dma73 <66970935+dma73@users.noreply.github.com>
2020-06-23 10:22:21 +0200

    Add tests for MidiTrack

--- test/midi-track-test.ts
+++ test/midi-track-test.ts
@@ -2,155 +2,46 @@ import { expect } from 'chai';
 import { MidiFile } from '../src/midi-file';
 import {describe, it} from 'mocha'
 import { MidiTrack } from '../src/midi-track';
+import { MidiEvent } from '../src/midi-event';
 
-describe('MidiFile: constants', () => {
-    it('HDR_CHUNKID should be MThd', () => {
-        const result = MidiFile.HDR_CHUNKID;
-        expect(result).to.equal('MThd');
+describe('MidiTrack: constants', () => {
+    it('START_BYTES should be MTrk', () => {
+        const result = MidiTrack.START_BYTES
+        expect(result.toString()).to.equal([0x4d, 0x54, 0x72, 0x6b].toString());
     });
-    it('HDR_CHUNK_SIZE should be \0\0\0\u0006', () => {
-        const result = MidiFile.HDR_CHUNK_SIZE;
-        expect(result).to.equal('\0\0\0\u0006');
-    });
-    it('HDR_TYPE0 should be \0\0', () => {
-        const result = MidiFile.HDR_TYPE0;
-        expect(result).to.equal('\0\0');
-    });
-    it('HDR_TYPE1 should be \0\u0001', () => {
-        const result = MidiFile.HDR_TYPE1;
-        expect(result).to.equal('\0\u0001');
-    });
-    it('DEFAULT_TICKS should be 128', () => {
-        const result = MidiFile.DEFAULT_TICKS;
-        expect(result).to.equal(128);
+    it('END_BYTES should be 00 FF 2F 00', () => {
+        const result = MidiTrack.END_BYTES;
+        expect(result.toString()).to.equal([0x00, 0xFF, 0x2F, 0x00].toString());
     });
 });
 
-describe('MidiFile: empty constructor', () => {
-    let midiFile = new MidiFile();
-    it('tracks should be empty', () => {
-        const result = midiFile.tracks;
-        expect(result.length).to.equal(0);
+describe('MidiTrack:constructor', () => {
+    
+    it('no args: track should be empty', () => {
+        let mt = new MidiTrack();
+        const result = mt.isEmpty();
+        expect(result).to.be.true;
     });
-    it('ticks should be defaulted to MidiFile.DEFAULT_TICKS', () => {
-        const result = midiFile.ticks;
-        expect(result).to.equal(MidiFile.DEFAULT_TICKS);
-    });
-    it('should be able to add an empty track', () => {
-        midiFile.addTrack();
-        const result = midiFile.tracks.length;
-        expect(result).to.equal(1);
-    });
-    it('should be able to add an exisiting track', () => {
-        const track = new MidiTrack();
-        midiFile.addTrack(track);
-        expect(midiFile.tracks.length).to.equal(2);
-        expect(midiFile.tracks[1]).to.equal(track);
+    it('non empty track as arg, events should be copied', () => {
+        let mt = new MidiTrack();
+        mt.addNote(1,60,128,20, 80);
+        const mt2 = new MidiTrack(mt);
+        const result = mt2.isEmpty();
+        expect(result).to.be.false;
     });
 });
-describe('MidiFile: constructor', () => {
-    let midiFile = new MidiFile();
-    midiFile.ticks = 200;
-    const track = new MidiTrack();
-    midiFile.addTrack(track);
-    let midiFile2 = new MidiFile(midiFile);
-    it('tracks should not be empty', () => {
-        expect(midiFile2.tracks.length).to.equal(midiFile.tracks.length);
+describe('MidiTrack:addEvent', () => {
+    it('track should not be empty', () => {
+        let mt = new MidiTrack();
+        mt.addEvent(new MidiEvent(MidiEvent.NOTE_ON, 0, 1, 60, 80));
+        const result = mt.isEmpty();
+        expect(result).to.be.false;
     });
-    it('ticks should be the same value as passed midifile', () => {
-        expect(midiFile2.ticks).to.equal(midiFile.ticks);
-        expect(midiFile2.ticks).to.equal(200);
+    it('fluid syntax should supported', () => {
+        let mt = new MidiTrack();
+        mt.addEvent(new MidiEvent(MidiEvent.NOTE_ON, 0, 1, 60, 80))
+            .addEvent(new MidiEvent(MidiEvent.NOTE_OFF, 0, 1, 60, 80));
+        const result = mt.isEmpty();
+        expect(result).to.be.false;
     });
 });
-describe('MidiFile: checkTicks - Ticks per beat must be an integer between 1 and 32767', () => {
-    let midiFile = new MidiFile();
-    it('Valid should not throw exception', () => {
-        expect(midiFile.checkTicks(10000)).to.be.true;
-    })
-})
-describe('MidiFile: checkAbove - Value should be above limit', () => {
-    let midiFile = new MidiFile();
-    it('Lower than 1 must throw exception', () => {
-        expect(() => midiFile.checkAbove(0,0, MidiFile.TICK_ERROR)).to.throw(MidiFile.TICK_ERROR);
-    })
-    it('Valid should not throw exception', () => {
-        expect(midiFile.checkAbove(10000,0, MidiFile.TICK_ERROR)).to.be.true;
-    })
-})
-describe('MidiFile: checkBelow - value must be below limit, otherwise throw error', () => {
-    let midiFile = new MidiFile();
-    it('Greater than 32767  must throw exception', () => {
-        expect(() => midiFile.checkBelow(32768,32768, MidiFile.TICK_ERROR)).to.throw(MidiFile.TICK_ERROR);
-    })
-    it('Valid should not throw exception', () => {
-        expect(midiFile.checkBelow(10000,32768, MidiFile.TICK_ERROR)).to.be.true;
-    })
-})
-describe('MidiFile: checkInteger - value should be an integer otherwise throw error', () => {
-    let midiFile = new MidiFile();
-    it('Non integer must throw exception', () => {
-        expect(() => midiFile.checkInteger(10.554, MidiFile.TICK_ERROR)).to.throw(MidiFile.TICK_ERROR);
-    })
-    it('Valid should not throw exception', () => {
-        expect(midiFile.checkTicks(10000)).to.be.true;
-    })
-})
-describe('MidiFile: setTicks - should only change the value if valid', () => {
-    it('Invalid value should not be accepted', () => {
-        let midiFile = new MidiFile();
-        try{
-            midiFile.setTicks(-2);
-        } catch {}
-        expect(midiFile.ticks).to.equal(MidiFile.DEFAULT_TICKS);
-    })
-    it('Valid value should be accepted', () => {
-        let midiFile = new MidiFile();
-        try{
-            midiFile.setTicks(256);
-        } catch {}
-        expect(midiFile.ticks).to.equal(256);
-    })
-})
-
-describe('MidiFile: toBytes - should only change the value if valid', () => {
-    it('single track', () => {
-        let midiFile = getSampleFile(1);
-        let hex = toHexString(midiFile.toBytes());
-        expect(hex).to.equal('4d 54 68 64 00 00 00 06 00 00 00 01 00 80 4d' +
-        ' 54 72 6b 00 00 00 0d 00 91 3c 50 81 00 81 3c 50 00 ff 2f 00 ');
-    })
-    it('two tracks', () => {
-        let midiFile = getSampleFile(2);
-        let hex = toHexString(midiFile.toBytes());
-        expect(hex).to.equal('4d 54 68 64 00 00 00 06 00 01 00 02 00 80 4d' +
-         ' 54 72 6b 00 00 00 0d 00 91 3c 50 81 00 81 3c 50 00 ff 2f 00 4d 54' +
-          ' 72 6b 00 00 00 0d 00 91 3c 50 81 00 81 3c 50 00 ff 2f 00 ');
-    })
-})
-describe('MidiFile: fromBytes - reimporting exported file should return the same midi file', () => {
-    it('single track file', () => {
-        let midiFile = getSampleFile(1);
-        let bytes = midiFile.toBytes();
-        let hex = toHexString(bytes);
-        let midiFile2 = MidiFile.fromBytes(Buffer.from(bytes,'binary'));
-        let hex2 = toHexString(midiFile2.toBytes());
-        expect(hex2).to.equal(hex);
-    })
-})
-function getSampleFile(tracks: number): MidiFile {
-    let midiFile = new MidiFile();
-    let track = new MidiTrack();
-    track.addNote(1,60, 128, 0, 80);
-    while (tracks > 0) {
-        midiFile.addTrack(track);
-        tracks --;
-    } 
-    return midiFile;
-}
-function toHexString(input: string):string{
-    let hex = '';
-    Buffer.from(input, 'binary').forEach((value) => {
-        hex += (value.toString(16).length === 1 ? 0 + value.toString(16) : value.toString(16)) + ' ';
-    });
-    return hex;
-} 
